87e9dc4245ff1c201025202b59190c3d0fb85b1c
yaellizarraga <yaellizarraga@outlook.es>
2020-09-23 01:25:44 -0600

    adding fibonacci sequence

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,31 @@
-console.log('index');
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fibonacci = (n) => {
+    let a = 0;
+    let b = 1;
+    for (let x = 0; x < n; x++) {
+        let tmp = a;
+        a = b;
+        b = tmp + b;
+        console.log(a);
+    }
+};
+const main = (config) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(config);
+    fibonacci(10);
+});
+const config = {
+    color: 'GREEN',
+    user: 'yaellizarraga'
+};
+main(config);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('index');
+export interface IConfig {
+    color: String;
+    user: String;
+}
+
+const fibonacci: any = (n: number) => {
+    let a: number = 0;
+    let b: number = 1;
+    for (let x = 0; x < n; x++) {
+        let tmp = a;
+        a = b;
+        b = tmp + b;
+        console.log(a);
+    }
+};
+
+const main = async (config: IConfig) => {
+    console.log(config);
+    fibonacci(10);
+};
+
+
+const config: IConfig = {
+    color: 'GREEN',
+    user: 'yaellizarraga'
+};
+
+main(config);
